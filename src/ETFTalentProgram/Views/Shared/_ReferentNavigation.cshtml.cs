9bec59ca9c1631e0d7d8d09d858eb488f3386afe
Muhamed2805 <muhamedkaramujic1@gmail.com>
2026-06-14 20:48:22 +0200

    header popravljen, spusteni tabovi ispod naslova

---
+++ src/ETFTalentProgram/Views/Shared/_ReferentNavigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ETFTalentProgram.Views.Shared
+{
+    public class _ReferentNavigationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
